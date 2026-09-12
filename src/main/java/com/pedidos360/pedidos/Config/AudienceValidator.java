e9ef47af8fc1c314367bc8e68a03ca6ba1400f61
victorlinares100 <linares28072004@gmail.com>
2026-09-11 22:15:31 -0300

    Correcion de validador de tokens

--- src/main/java/com/pedidos360/pedidos/Config/AudienceValidator.java
+++ src/main/java/com/pedidos360/pedidos/Config/AudienceValidator.java
@@ -1,9 +1,15 @@
 package com.pedidos360.pedidos.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
 
@@ -20,5 +26,17 @@ public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
         }
         OAuth2Error error = new OAuth2Error("invalid_token", "El token no tiene la audiencia esperada", null);
         return OAuth2TokenValidatorResult.failure(error);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+    String issuerUri = "https://login.microsoftonline.com/064d1db6-6358-4f36-bbb6-11dfe3537831/v2.0";
+    NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromIssuerLocation(issuerUri);
+
+    OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri);
+    OAuth2TokenValidator<Jwt> withAudience = new AudienceValidator("api://8f72ba0f-8036-40e2-9465-c8ae3891444d");
+    jwtDecoder.setJwtValidator(new DelegatingOAuth2TokenValidator<>(withIssuer, withAudience));
+
+    return jwtDecoder;
     }
 }
--- src/main/java/com/pedidos360/pedidos/Config/SecurityConfig.java
+++ src/main/java/com/pedidos360/pedidos/Config/SecurityConfig.java
@@ -4,15 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
-import org.springframework.security.oauth2.core.OAuth2TokenValidator;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.JwtValidators;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.security.oauth2.jwt.Jwt;
 
 @Configuration
 public class SecurityConfig {
@@ -31,14 +24,4 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public JwtDecoder jwtDecoder() {
-    String issuerUri = "https://login.microsoftonline.com/064d1db6-6358-4f36-bbb6-11dfe3537831/v2.0";
-    NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromIssuerLocation(issuerUri);
-
-    OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri);
-    OAuth2TokenValidator<Jwt> withAudience = new AudienceValidator("api://8f72ba0f-8036-40e2-9465-c8ae3891444d");
-    jwtDecoder.setJwtValidator(new DelegatingOAuth2TokenValidator<>(withIssuer, withAudience));
-
-    return jwtDecoder;
-}}
+}
